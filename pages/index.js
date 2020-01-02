cf5ca674a4d680e2cd98e6ac36c1903edc96531c
ryszard-put <ryszard.poznanski0@gmail.com>
2020-01-02 20:11:02 +0100

    Add main section

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import '../styles/about.scss';
+import MainLayout from '../components/Layouts/MainLayout';
 
 const Home = () => {
-  return <div>Hello Next JS</div>;
+  return (
+    <MainLayout>
+      <main className='about_page'>
+        <h1>Lorem ipsum dolor sit amed</h1>
+      </main>
+      <h1>adasdasd</h1>
+    </MainLayout>
+  );
 };
 
 export default Home;
