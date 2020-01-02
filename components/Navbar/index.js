fe8bf91c46cb923337d256a73c34bb64f0c7256b
ryszard-put <ryszard.poznanski0@gmail.com>
2020-01-02 20:09:58 +0100

    Add conditional styling

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,66 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { ReactSVG } from 'react-svg';
 
 import './navbar.scss';
+import useScrollPosition from '../../hooks/useScrollPosition';
 
 const Navbar = () => {
-  const [isMenuActive, setIsMenuActive] = useState(true);
+  const [isMenuActive, setIsMenuActive] = useState(false);
+  const scrollPosition = useScrollPosition();
+  const { route } = useRouter();
+
   function toggleMenu() {
     console.log(window.pageYOffset);
     setIsMenuActive(state => !state);
   }
+
+  const headerClasses = [
+    'navbar__header',
+    route == '/' && scrollPosition == 0 && 'navbar__header--transparent',
+    route == '/' &&
+      !isMenuActive &&
+      scrollPosition == 0 &&
+      'navbar__header--reversed'
+  ]
+    .filter(s => s)
+    .join(' ');
+
   return (
-    <nav className='navbar'>
-      <div className='navbar__header'>
-        {/* <img
-          className='navbar__header_logo'
-          src='/assets/icons/logo-dark.svg'
-          alt='logo-light'
-        /> */}
+    <div className='navbar'>
+      <div className={headerClasses}>
         <ReactSVG
           src='/assets/icons/logo.svg'
+          className='navbar__header_logo'
+        />
+        <div
           className={[
-            'navbar__header_logo',
-            isMenuActive && 'navbar__header_logo--reversed'
+            'navbar__header_button',
+            isMenuActive && 'navbar__header_button--animated'
           ]
             .filter(s => s)
             .join(' ')}
-        />
-        <img
-          className='navbar__header_button'
-          src='/assets/icons/menu.svg'
-          alt='menu_burger'
           onClick={toggleMenu}
-        />
+        >
+          <div></div>
+        </div>
       </div>
-      <ul
-        className={['navbar__list', isMenuActive && 'navbar__list--active']
+      <nav
+        className={[
+          'navbar__navigation',
+          isMenuActive && 'navbar__navigation--active'
+        ]
           .filter(s => s)
           .join(' ')}
       >
-        <li className='navbar__list_item'>
-          <Link href='/'>
-            <a>O nas</a>
-          </Link>
-        </li>
-        <li className='navbar__list_item'>
-          <Link href='/wydarzenia'>
-            <a>Wydarzenia</a>
-          </Link>
-        </li>
-        <li className='navbar__list_item'>
-          <Link href='/blog'>
-            <a>Blog</a>
-          </Link>
-        </li>
-        <li className='navbar__list_item navbar__list_item--alternative'>
-          <a href='https://apps.akai.org.pl' target='_blank'>
-            Projekty
-          </a>
-        </li>
-      </ul>
-    </nav>
+        <ul className='navigation__list'>
+          <li className='navigation__list_item'>
+            <Link href='/'>
+              <a>O nas</a>
+            </Link>
+          </li>
+          <li className='navigation__list_item'>
+            <Link href='/wydarzenia'>
+              <a>Wydarzenia</a>
+            </Link>
+          </li>
+          <li className='navigation__list_item'>
+            <Link href='/blog'>
+              <a>Blog</a>
+            </Link>
+          </li>
+          <li className='navigation__list_item navigation__list_item--alternative'>
+            <a href='https://apps.akai.org.pl' target='_blank'>
+              Projekty
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
