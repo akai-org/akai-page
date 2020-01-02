b474e749f42255214ace615384ddf8b98409d783
ryszard-put <ryszard.poznanski0@gmail.com>
2020-01-02 14:13:38 +0100

    Wrap pages with MainLayout wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './global.scss';
+import '../styles/global.scss';
 import Placeholder from '../components/Placeholder';
+import MainLayout from '../components/Layouts/MainLayout';
 
 const App = props => {
   const { Component, pageProps } = props;
   return (
-    <>
-      {/* <Component {...pageProps} /> */}
-      <Placeholder />
-    </>
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
   );
 };
 
