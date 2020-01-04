02059e3ac6210098ca0a52877cb39fa7adad138a
marcol13 <56632321+marcol13@users.noreply.github.com>
2020-01-04 16:53:48 +0100

    Styling event component

--- pages/wydarzenia/index.js
+++ pages/wydarzenia/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import MainLayout from '../../components/Layouts/MainLayout';
+import Event from '../../components/Events/event'
+
+const contStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100vh"
+}
 
 const EventsHome = () => {
-  return <MainLayout>AKAI EVENTS INDEX PAGE</MainLayout>;
+  return(
+    
+      <MainLayout>AKAI EVENTS INDEX PAGE
+        <div className="cont" style={contStyle}>
+          <Event/>
+        </div>
+      </MainLayout>
+  );
 };
 
 export default EventsHome;
