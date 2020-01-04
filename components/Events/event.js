3530df3d229fa02c39b0c6eedd47407a5028a9ca
marcol13 <56632321+marcol13@users.noreply.github.com>
2020-01-04 15:44:54 +0100

    Add Events components

---
+++ components/Events/event.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './eventStyle.scss'
+
+const Event = () => {
+    
+    var objToday = new Date(),
+	weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+	dayOfWeek = weekday[objToday.getDay()],
+	domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
+	dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+	months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+	curMonth = months[objToday.getMonth()],
+	curYear = objToday.getFullYear(),
+	curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+	curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
+	curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
+	curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
+    var today = curHour + ":" + curMinute + "." + curSeconds + curMeridiem + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
+    return(
+        <div className="event-container">
+            <div className="event-picture">
+                <img src="" alt=""/>
+            </div>
+            <div className="event-info">
+                <div className="event-flex-title">
+                    <h3 className="event-title">Bardzo długi tytuł wydarzenia</h3>
+                    <div className="event-break"></div>
+                </div>
+                <div className="event-flex-details">
+                    <p className="event-place">Miejsce wydarzenia</p>
+                    <p className="event-date">{today}</p>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Event
